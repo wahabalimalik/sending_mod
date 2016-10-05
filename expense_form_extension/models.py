01aeb268b7f5c85bcd4c5a71a3663a7b93110f5a
wahabalimalik <wahabalimalik@gmail.com>
2016-10-05 20:21:18 +0500

    extending loan module

--- expense_form_extension/models.py
+++ expense_form_extension/models.py
@@ -81,17 +81,30 @@ class expense_form_extension_1(models.Model):
 class loan_management(models.Model):
 	_inherit = 'hr.expense.expense'
 
-	loan            = fields.Boolean()
-	loan_start_date = fields.Date('Start Date', required=True)
-	loan_end_date   = fields.Date('End Date', required=True)
-	installments    = fields.Integer(string="No of Installment")
-	loan_paid       = fields.Float(string="Loan Paid")
-	loan_remaining  = fields.Float(string="Loan Remaining")
+	loan             = fields.Boolean()
+	loan_start_date  = fields.Date('Start Date', required=True)
+	loan_end_date    = fields.Date('End Date', required=True)
+	installments     = fields.Integer(string="No of Installment")
+	loan_paid        = fields.Float(string="Loan Paid" ,compute="_compute_amount_paid")
+	loan_remaining   = fields.Float(string="Loan Remaining")
+	amount_per_month = fields.Float(string="Amount per Month")
+	pringle          = fields.One2many('loan.1122','cringle')
 
-	pringle         = fields.One2many('loan.1122','cringle')
+	@api.multi
+	def _compute_amount_paid(self):
+		self.loan_paid = sum(line.amount for line in self.pringle)
+
+	@api.onchange('installments')
+	def _onchange_amount_per_month(self):
+		if self.loan != False and self.installments != 0:
+			self.amount_per_month= self.advance / self.installments
+		elif self.loan != False and self.installments == 0:
+			self.amount_per_month= self.advance * 0
+		return
+
 
 	@api.onchange('advance','loan_paid')
-	@api.depends('advance','loan_paid','loan')
+	# @api.depends('advance','loan_paid','loan')
 	def _onchange_amount(self):
 		if self.loan != False:
 			self.loan_remaining= self.advance - self.loan_paid
@@ -107,17 +120,18 @@ class loan_management(models.Model):
 			end_date = str(x.slip_id.date_to)
 			start_date = dt.strptime(start_date[2:], "%y-%m-%d")
 			end_date   = dt.strptime(end_date[2:], "%y-%m-%d")
-			if x.date != False:
-				cc_date = dt.strptime(x.date[2:], "%y-%m-%d")
-				if x.code == 'BASIC' and start_date <= cc_date and cc_date<=end_date:
-					for y in self:
-						y.pringle.create({
-							'name'     : x.name,
-							'date'     : x.date,
-							'slip_id'  : x.slip_id.id,
-							'amount'   : x.amount,
-							'cringle'  : y.id
-							})
+			start_date_1  = dt.strptime(self.loan_start_date[2:], "%y-%m-%d")
+			end_date_1   = dt.strptime(self.loan_end_date[2:], "%y-%m-%d")
+			if x.code == 'loan_ded' and start_date <= start_date_1 and end_date_1<=end_date:
+				for y in self:
+					y.pringle.create({
+						'name'     : x.name,
+						'date'     : x.slip_id.date_from,
+						'slip_id'  : x.slip_id.id,
+						'amount'   : x.amount,
+						'cringle'  : y.id
+						})
+
 class loan_management_1122(models.Model):
 	_name = 'loan.1122'
 	name = fields.Char()
@@ -126,6 +140,41 @@ class loan_management_1122(models.Model):
 	amount = fields.Float()
 	cringle  = fields.Many2one('hr.expense.expense',
         ondelete='cascade')
-class loan_management_1(models.Model):
-	_inherit = 'hr.payslip.line'
-	date = fields.Date()
+
+class hr_employee(models.Model):
+    _inherit = 'hr.employee'
+    @api.model
+    def loan_ded(self, payslip):
+        duration = 0.0
+        tsheet_obj = self.env['hr.expense.expense']
+        timesheets = tsheet_obj.search([('employee_id', '=', self.id), 
+            ('loan_start_date', '>=', payslip.date_from), ('loan_end_date', '<=', payslip.date_to)])
+        # if self.user_id: #also add timesheets if only user is selected 
+        #     tsheet_add = tsheet_obj.search([('user_id', '=', self.user_id.id), 
+        #     ('date', '>=', payslip.date_from), ('date', '<=', payslip.date_to), ('officer', '=', False)])
+        #     timesheets += tsheet_add
+        for tsheet in timesheets: #counting duration from timesheets
+            duration += tsheet.amount_per_month 
+
+
+        return duration
+
+    # @api.model
+    # def loan_ded(self, payslip):
+    #     # duration = 0.0
+        
+
+
+
+
+
+
+        # tsheet_obj = self.env['hr.expense.expense']
+        # timesheets = tsheet_obj.search([('employee_id', '=', self.id), 
+        #     ('loan_start_date', '>=', payslip.date_from), ('loan_end_date', '<=', payslip.date_to)])
+        # for  x in timesheets:
+        # 	print "ccccccccccccccccccccccccc"
+        # 	timesheet += x.amount_per_month
+
+        # return timesheet
+        # 
